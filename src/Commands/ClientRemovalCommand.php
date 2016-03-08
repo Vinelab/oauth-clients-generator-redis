ef543bd18b58961e6c84efda3a70e0b32bf29cd4
Charalampos Raftopoulos <harrisrafto@gmail.com>
2016-03-08 19:00:42 +0200

    rename client deletor command to client removal command

--- src/Commands/ClientRemovalCommand.php
+++ src/Commands/ClientRemovalCommand.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ClientDeletorCommand extends Command
+class ClientRemovalCommand extends Command
 {
     /**
      * Command for deleting specific client.
